09edcd95e4f15d9793bea90188dbd17a9a095648
HaJunYoo <hajuny129@gmail.com>
2023-05-16 22:28:54 +0900

    ✨ feat: allowed_host *

--- main/settings.py
+++ main/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 # CORS_ALLOWED_ORIGINS = [
 # 	# 허용할 Origin 추가
 #     "https://example.com",
