cd7a8b75d63a7b96c468a361136924842cf899a3
HaJunYoo <hajuny129@gmail.com>
2023-05-24 00:18:39 +0900

    ✨ feat: urls.py fix

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/', check_connection, name='check-connection'),
+    path('', check_connection, name='check-connection'),
     # 다른 URL 패턴들을 여기에 추가할 수 있습니다.
 ]
