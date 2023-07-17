38df18ecbacfc6d52f07e7b203a363480dd7018c
HaJunYoo <hajuny129@gmail.com>
2023-07-17 16:38:06 +0900

    ✨ feat: model change

--- videostream/models.py
+++ videostream/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Session(models.Model):
     session_id = models.IntegerField(primary_key=True, default=1)
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
 
 class SessionData(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
