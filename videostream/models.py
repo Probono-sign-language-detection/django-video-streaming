ee5e63ae8bb482da19140fbea74d1871a897428b
HaJunYoo <hajuny129@gmail.com>
2023-07-17 16:10:04 +0900

    ✨ feat: model change

--- videostream/models.py
+++ videostream/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Session(models.Model):
-    session_id = models.IntegerField(primary_key=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    session_id = models.IntegerField(primary_key=True, default=1)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
 class SessionData(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     id = models.IntegerField(primary_key=True)
-    word = models.CharField(max_length=100)
+    word = models.CharField(max_length=100, blank=True)
 
     class Meta:
         unique_together = (("session", "id"),)
